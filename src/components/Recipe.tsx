98e355bb73cc113481d1b21e11a01929cef8a71c
Barton NIU <barton.niu@gmail.com>
2021-07-29 20:06:49 +1000

    styles: update recipe screen styles

--- src/components/Recipe.tsx
+++ src/components/Recipe.tsx
@@ -8,52 +8,73 @@ function Recipe({ setIsModalOpen, dataProps }: any) {
   const { status, data, error } = dataProps;
 
   return (
-    <div className='bg-gray-200 fixed z-50 h-full w-full overflow-auto flex justify-center align-middle'>
+    <div className='bg-yellow-100 dark:bg-gray-800  dark:text-white fixed z-50 h-full w-full overflow-auto flex justify-center align-middle'>
       <div
-        className='text-3xl absolute top-5 right-10 cursor-pointer'
+        className='bg-blue-300 text-white text-3xl fixed top-5 right-5 cursor-pointer '
         onClick={handleClick}
       >
         <RiCloseFill />
       </div>
       {status === "success" ? (
         <div className='recipe-cont'>
-          <h3>{data.data.result.result.list[0].name}</h3>
-          <p>{data.data.result.result.list[0].content}</p>
-          <img
-            src={data.data.result.result.list[0].pic}
-            alt={data.data.result.result.list[0].name}
-          />
-          <div className='materials'>
-            <ul>
-              {data.data.result.result.list[0].material.map(
-                (item: any, index: any) => (
-                  <li key={index}>
-                    <span>{item.mname} </span>
-                    <span>{item.amount}</span>
-                  </li>
-                )
-              )}
-            </ul>
+          <div className='w-full h-auto'>
+            <img
+              className='w-full h-full object-cover'
+              src={data.data.result.result.list[0].pic}
+              alt={data.data.result.result.list[0].name}
+            />
           </div>
-          <div className='processes'>
-            <ul>
-              {data.data.result.result.list[0].process.map(
-                (item: any, index: any) => (
-                  <li key={index}>
-                    <span>{item.pcontent} </span>
-                    <span>
-                      <img src={item.pic} alt='process' />
-                    </span>
-                  </li>
-                )
-              )}
-            </ul>
+          <div className='p-5 '>
+            <div className='text-2xl font-medium pb-5'>
+              {data.data.result.result.list[0].name}
+            </div>
+            <div className='text-justify '>
+              {data.data.result.result.list[0].content}
+            </div>
+
+            <div className='pt-5'>
+              <div className='font-semibold pb-2 text-left text-lg '>用料</div>
+              <ul>
+                {data.data.result.result.list[0].material.map(
+                  (item: any, index: any) => (
+                    <li
+                      key={index}
+                      className='text-ml flex justify-between align-middle py-2 border-b border-gray-300'
+                    >
+                      <span>{item.mname} </span>
+                      <span>{item.amount}</span>
+                    </li>
+                  )
+                )}
+              </ul>
+            </div>
+            <div className='pt-5'>
+              <ul>
+                {data.data.result.result.list[0].process.map(
+                  (item: any, index: any) => (
+                    <li key={index} className='border-b border-gray-300 py-2'>
+                      <div className='font-semibold pb-2 text-left  '>
+                        步骤 {index + 1}
+                      </div>
+                      <div className='w-full h-auto'>
+                        <img
+                          className='w-full h-full'
+                          src={item.pic}
+                          alt='process'
+                        />
+                      </div>
+                      <div className='text-left py-2'>{item.pcontent} </div>
+                    </li>
+                  )
+                )}
+              </ul>
+            </div>
           </div>
         </div>
       ) : error ? (
-        <div>{error}</div>
+        <div>{error.message}</div>
       ) : (
-        <div>Loading...</div>
+        <div>加载中...</div>
       )}
     </div>
   );
