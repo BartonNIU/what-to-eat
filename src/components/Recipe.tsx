be7ad4a7215c59b2300e55466cd648ab60a277c0
Barton NIU <barton.niu@gmail.com>
2021-07-29 20:12:52 +1000

    feat: add request exception handler

--- src/components/Recipe.tsx
+++ src/components/Recipe.tsx
@@ -16,61 +16,67 @@ function Recipe({ setIsModalOpen, dataProps }: any) {
         <RiCloseFill />
       </div>
       {status === "success" ? (
-        <div className='recipe-cont'>
-          <div className='w-full h-auto'>
-            <img
-              className='w-full h-full object-cover'
-              src={data.data.result.result.list[0].pic}
-              alt={data.data.result.result.list[0].name}
-            />
-          </div>
-          <div className='p-5 '>
-            <div className='text-2xl font-medium pb-5'>
-              {data.data.result.result.list[0].name}
+        data.data.result.result ? (
+          <div className='recipe-cont'>
+            <div className='w-full h-auto'>
+              <img
+                className='w-full h-full object-cover'
+                src={data.data.result.result.list[0].pic}
+                alt={data.data.result.result.list[0].name}
+              />
             </div>
-            <div className='text-justify '>
-              {data.data.result.result.list[0].content}
-            </div>
+            <div className='p-5 '>
+              <div className='text-2xl font-medium pb-5'>
+                {data.data.result.result.list[0].name}
+              </div>
+              <div className='text-justify '>
+                {data.data.result.result.list[0].content}
+              </div>
 
-            <div className='pt-5'>
-              <div className='font-semibold pb-2 text-left text-lg '>用料</div>
-              <ul>
-                {data.data.result.result.list[0].material.map(
-                  (item: any, index: any) => (
-                    <li
-                      key={index}
-                      className='text-ml flex justify-between align-middle py-2 border-b border-gray-300'
-                    >
-                      <span>{item.mname} </span>
-                      <span>{item.amount}</span>
-                    </li>
-                  )
-                )}
-              </ul>
-            </div>
-            <div className='pt-5'>
-              <ul>
-                {data.data.result.result.list[0].process.map(
-                  (item: any, index: any) => (
-                    <li key={index} className='border-b border-gray-300 py-2'>
-                      <div className='font-semibold pb-2 text-left  '>
-                        步骤 {index + 1}
-                      </div>
-                      <div className='w-full h-auto'>
-                        <img
-                          className='w-full h-full'
-                          src={item.pic}
-                          alt='process'
-                        />
-                      </div>
-                      <div className='text-left py-2'>{item.pcontent} </div>
-                    </li>
-                  )
-                )}
-              </ul>
+              <div className='pt-5'>
+                <div className='font-semibold pb-2 text-left text-lg '>
+                  用料
+                </div>
+                <ul>
+                  {data.data.result.result.list[0].material.map(
+                    (item: any, index: any) => (
+                      <li
+                        key={index}
+                        className='text-ml flex justify-between align-middle py-2 border-b border-gray-300'
+                      >
+                        <span>{item.mname} </span>
+                        <span>{item.amount}</span>
+                      </li>
+                    )
+                  )}
+                </ul>
+              </div>
+              <div className='pt-5'>
+                <ul>
+                  {data.data.result.result.list[0].process.map(
+                    (item: any, index: any) => (
+                      <li key={index} className='border-b border-gray-300 py-2'>
+                        <div className='font-semibold pb-2 text-left  '>
+                          步骤 {index + 1}
+                        </div>
+                        <div className='w-full h-auto'>
+                          <img
+                            className='w-full h-full'
+                            src={item.pic}
+                            alt='process'
+                          />
+                        </div>
+                        <div className='text-left py-2'>{item.pcontent} </div>
+                      </li>
+                    )
+                  )}
+                </ul>
+              </div>
             </div>
           </div>
-        </div>
+        ) : (
+          <div>啊，没有找到相关的菜谱</div>
+        )
       ) : error ? (
         <div>{error.message}</div>
       ) : (
