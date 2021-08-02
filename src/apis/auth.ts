7949abf76be9d9a09fd7104ca09821f9eed92f30
Barton NIU <barton.niu@gmail.com>
2021-08-02 23:02:31 +1000

    feat: update auth function

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -1,9 +1,9 @@
 import { api } from "../services/api";
 
 export const login = (name: string, password: string) => {
-  return api.post("/login", { name, password });
+  return api.post("/auth/login", { email: name, password });
 };
 
 export const register = (name: string, password: string) => {
-  return api.post("/register", { name, password });
+  return api.post("/auth/register", { email: name, password });
 };
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { loginSchema } from "../utils/formSchema";
 import { useHistory } from "react-router-dom";
+import { useQuery } from "react-query";
+import { login } from "../apis/auth";
 
 function Login() {
   const history = useHistory();
@@ -14,9 +16,28 @@ function Login() {
   } = useForm({
     resolver: yupResolver(loginSchema),
   });
+  const [formData, setFormData] = useState({ email: "", password: "" });
+  const [error, setError] = useState("");
 
-  const onSubmit = (data: any) => {
+  // const { status, data, error, refetch } = useQuery(
+  //   "login",
+  //   () => login(formData.email, formData.password),
+  //   {
+  //     enabled: !!(formData.email && formData.password),
+  //   }
+  // );
+
+  // console.log(status, data, error);
+
+  const onSubmit = async (data: any) => {
     console.log(data);
+    try {
+      const result = await login(data.email, data.password);
+      console.log(result);
+    } catch (error) {
+      console.error(error.message, error.response);
+      setError(error.response?.data.msg || error.message);
+    }
   };
 
   const handleClick = () => {
@@ -28,6 +49,7 @@ function Login() {
   return (
     <div className='h-full flex flex-col justify-center items-center'>
       <div className='text-2xl font-bold mb-5'>登录</div>
+      <div className='text-red-500 mb-3'>{error}</div>
       <form onSubmit={handleSubmit(onSubmit)}>
         <input
           className='border-b-2 p-3 m-2'
