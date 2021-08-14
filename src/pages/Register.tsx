d51b3e4c0290d3b108635e8c868824a9c15e0063
Barton NIU <barton.niu@gmail.com>
2021-08-14 19:50:38 +1000

    chore: add loading status for register

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -18,71 +18,74 @@ function Register() {
   });
 
   const [error, setError] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
 
-  const onSubmit = async (data: any) => {
-    console.log(data);
-    try {
-      const result = await registerRequest(data.email, data.password);
-      console.log(result);
-      history.push("/login");
-    } catch (error) {
-      console.error(error.message, error.response);
-      setError(error.response?.data.msg || error.message);
-    }
-  };
+    const onSubmit = async (data: any) => {
+      console.log(data);
+      setIsLoading(true);
+      try {
+        const result = await registerRequest(data.email, data.password);
+        console.log(result);
+        history.push("/login");
+      } catch (error) {
+        console.error(error.message, error.response);
+        setError(error.response?.data.msg || error.message);
+      }
+      setIsLoading(false);
+    };
 
-  const handleClick = () => {
-    history.push({
-      pathname: "/login",
-    });
-  };
+    const handleClick = () => {
+      history.push({
+        pathname: "/login",
+      });
+    };
 
-  return (
-    <>
-      <Header />
-      <div className=' h-screen flex flex-col justify-center items-center'>
-        <div className='dark:text-white text-2xl font-bold mb-5'>注册</div>
-        <div className='text-red-500 mb-3'>{error}</div>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <input
-            className='border-b-2 p-3 m-2'
-            placeholder='你的邮箱...'
-            {...register("email")}
-          />
-          <div className='text-red-500 text-sm mb-3'>
-            {errors.email?.message}
+    return (
+      <>
+        <Header />
+        <div className=' h-screen flex flex-col justify-center items-center'>
+          <div className='dark:text-white text-2xl font-bold mb-5'>注册</div>
+          <div className='text-red-500 mb-3'>{error}</div>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <input
+              className='border-b-2 p-3 m-2'
+              placeholder='你的邮箱...'
+              {...register("email")}
+            />
+            <div className='text-red-500 text-sm mb-3'>
+              {errors.email?.message}
+            </div>
+            <input
+              className='border-b-2 p-3 m-2'
+              type='password'
+              placeholder='设置密码...'
+              {...register("password")}
+            />
+            <div className='text-red-500 text-sm mb-3'>
+              {errors.password?.message}
+            </div>
+            <input
+              className='border-b-2 p-3 m-2'
+              type='password'
+              placeholder='再次输入密码...'
+              {...register("confirmPassword")}
+            />
+            <div className='text-red-500 text-sm mb-3'>
+              {errors.confirmPassword?.message}
+            </div>
+            <button className='bg-blue-500 hover:bg-blue-600 text-white shadow-lg px-10 py-3'>
+              {isLoading ? "注册中..." : "注册"}
+            </button>
+          </form>
+          <div
+            className='dark:text-white text-sm pt-5 border-b border-gray-500'
+            onClick={handleClick}
+          >
+            已注册？点此登录
           </div>
-          <input
-            className='border-b-2 p-3 m-2'
-            type='password'
-            placeholder='设置密码...'
-            {...register("password")}
-          />
-          <div className='text-red-500 text-sm mb-3'>
-            {errors.password?.message}
-          </div>
-          <input
-            className='border-b-2 p-3 m-2'
-            type='password'
-            placeholder='再次输入密码...'
-            {...register("confirmPassword")}
-          />
-          <div className='text-red-500 text-sm mb-3'>
-            {errors.confirmPassword?.message}
-          </div>
-          <button className='bg-blue-500 hover:bg-blue-600 text-white shadow-lg px-10 py-3'>
-            注册
-          </button>
-        </form>
-        <div
-          className='dark:text-white text-sm pt-5 border-b border-gray-500'
-          onClick={handleClick}
-        >
-          已注册？点此登录
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
 }
 
 export default Register;
