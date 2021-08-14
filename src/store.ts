1c25566ec2bd92689fa2e4f8db6c0507341a2dbf
Barton NIU <barton.niu@gmail.com>
2021-08-14 12:10:02 +1000

    feat: add localstorage for store state

--- src/store.ts
+++ src/store.ts
@@ -3,13 +3,39 @@ import menusSlice from "./redux/menusSlice";
 import settingsSlice from "./redux/settingsSlice";
 import usersSlice from "./redux/usersSlice";
 
+const loadFromLocalStorage = () => {
+  try {
+    const serializedState = localStorage.getItem("what-to-eat");
+    if (serializedState === null) {
+      return undefined;
+    }
+    return JSON.parse(serializedState);
+  } catch (error) {
+    console.error(error);
+
+    return undefined;
+  }
+};
+
+const saveToLocalStorage = (state: RootState) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem("what-to-eat", serializedState);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 export const store = configureStore({
   reducer: {
     settings: settingsSlice,
     menus: menusSlice,
     users: usersSlice,
   },
+  preloadedState: loadFromLocalStorage(),
 });
+
+store.subscribe(() => saveToLocalStorage(store.getState()));
 
 export type RootState = ReturnType<typeof store.getState>;
 
